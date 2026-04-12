3fc6766d1be6de4d29e8fa8ed65a90022b698645
gwidopw <gwidoputra@gmail.com>
2026-04-13 01:14:50 +0700

    fix static build index

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,19 @@
 import { defineConfig } from 'vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
 import contentCollections from '@content-collections/vite'
 
-const config = defineConfig({
+export default defineConfig({
   plugins: [
     contentCollections(),
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
-    netlify(),
-    tanstackStart(),
     viteReact(),
   ],
+  build: {
+    outDir: 'dist',
+  },
 })
-
-export default config
