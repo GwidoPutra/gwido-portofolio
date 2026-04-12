53db2f4a86bd7a06064965a0854f7f61cdebde6b
gwidopw <gwidoputra@gmail.com>
2026-04-13 01:41:09 +0700

    fix tinggal dikit plis

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen' // File ini otomatis terbuat oleh TanStack
+import './styles.css' // Sesuaikan dengan file CSS kamu
+
+const router = createRouter({ routeTree })
+
+const rootElement = document.getElementById('root')!
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  )
+}
